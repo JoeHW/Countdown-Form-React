49beedda31234b89f943d32ba8e177ba46396c59
Joe Williamson <jhwilliamson@outlook.com>
2017-09-01 18:56:12 -0400

    Basic structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,23 @@
 import React from 'react';
+import './App.css';
 
 class App extends React.Component {
  render() {
    return (
-   <div>Countdown App Component</div>
+    <div className='App'>
+     <div className='App-title'>Countdown to December 25 2017</div>
+     <div>
+      <div className='clock-days'>14 Days</div>
+      <div className='clock-hours'>23 Hours</div>
+      <div className='clock-minutes'>56 Minutes</div>
+      <div className='clock-seconds'>09 Seconds</div>
+     </div>
+     <div>
+     <input placeholder='new date'/>
+     <button>Submit</button>
+     </div>
+     </div>
+
   )
  }
 }

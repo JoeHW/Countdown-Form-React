abc41935f91abf83b013e3be96245abe37128339
Joe Williamson <jhwilliamson@outlook.com>
2017-09-04 13:22:43 -0400

    Timer

--- src/Stopwatch.jsx
+++ src/Stopwatch.jsx
@@ -6,24 +6,33 @@ class Stopwatch extends Component {
  constructor(props){
    super(props);
    this.state = {
-     seconds: this.props.seconds
+     seconds: this.props.seconds,
+     newSecs: ""
 }
 }
 
  componentWillMount = () => this.setTime(this.props.seconds);
 
- componentDidMount = () => setInterval(() => this.setTime(this.props.seconds), 1000);
+ componentDidMount = () => setInterval(() => this.setTimeAgain(this.state.newSecs), 1000);
 
 setTime = (secs) => {
-this.setState({seconds: this.props.seconds -1})
-}
+if(this.state.seconds > 0){
+this.setState({newSecs: this.props.seconds -1})
+console.log(this.state);
+}}
+
+setTimeAgain = (secs) => {
+if(this.state.newSecs > 0){
+this.setState({newSecs: this.state.newSecs -1})
+console.log(this.state);
+}}
 
 render() {
  return (
     <div>
     <div className='stopwatch-seconds'>{this.props.seconds} seconds</div>
     <br/>
-    <div className='stopwatch-time'>{this.props.seconds}</div>
+    <div className='stopwatch-time'>{this.state.newSecs}</div>
     </div>
  )
 }
